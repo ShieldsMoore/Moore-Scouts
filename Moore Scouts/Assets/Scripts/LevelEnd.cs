de82f17afcba382a54b79aeff60dd005c801059a
ShieldsMoore <49412632+ShieldsMoore@users.noreply.github.com>
2019-04-08 17:18:59 -0400

    Clean Up
    removed old tile set
    reset win screen with new graphics

--- Moore Scouts/Assets/Scripts/LevelEnd.cs
+++ Moore Scouts/Assets/Scripts/LevelEnd.cs
@@ -10,6 +10,11 @@ public class LevelEnd : MonoBehaviour {
     public PlayerController pc;
     public Rigidbody2D rb2D;
     public Animator myanim;
+ 
+
+    public string Main;
+    public string Next;
+   
 
 	// Use this for initialization
 	void Start () {
@@ -21,21 +26,43 @@ public class LevelEnd : MonoBehaviour {
 		
 	}
 
-    public IEnumerator LevelEndCo()
-    {
-        myanim.SetBool("Win", true);
-        pc.enabled = false;
-        rb2D.velocity = Vector2.zero;
-        youwin.SetActive(true);
-        yield return new WaitForSeconds(2f);
-        SceneManager.LoadScene(levelToLoad);
-    }
+    //public IEnumerator LevelEndCo()
+   // {
+   //     myanim.SetBool("Win", true);
+   //     pc.enabled = false;
+   //     rb2D.velocity = Vector2.zero;
+   //     youwin.SetActive(true);
+        //yield return new WaitForSeconds(2f);
+        //SceneManager.LoadScene(levelToLoad);
+    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            StartCoroutine("LevelEndCo");
+            myanim.SetBool("Win", true);
+            pc.enabled = false;
+            rb2D.velocity = Vector2.zero;
+            youwin.SetActive(true);
+           
+           
+
+            //StartCoroutine("LevelEndCo");
         }
+    }
+
+    public void VisitMoore()
+    {
+        Application.OpenURL("https://moore.edu/");
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(Next);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(Main);
     }
 }
